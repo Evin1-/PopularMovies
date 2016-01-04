787c1bea0ee9e386409356fde25fd18b0df0d816
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-03 21:01:17 -0500

    Add menu layout and settings

--- app/src/main/java/mx/evin/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/mx/evin/udacity/popularmovies/MainActivity.java
@@ -2,12 +2,41 @@ package mx.evin.udacity.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "PopularMoviesTAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.toggle:
+                if (item.getTitle().equals("Order by popularity")){
+                    item.setTitle(getString(R.string.menu_toggle_rating));
+                }else{
+                    item.setTitle(getString(R.string.menu_toggle_popularity));
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
