54fbe2aa02290d9a3dd186bfc8d9d1f7fe6a9410
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-04 00:23:08 -0500

    Retrofit working and Interceptors set up

--- app/src/main/java/mx/evin/udacity/popularmovies/RetrieveMovies.java
+++ app/src/main/java/mx/evin/udacity/popularmovies/RetrieveMovies.java
@@ -5,8 +5,11 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.ResponseBody;
+import com.squareup.okhttp.logging.HttpLoggingInterceptor;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,11 +27,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import mx.evin.udacity.popularmovies.entities.Page;
+import mx.evin.udacity.popularmovies.entities.Result;
+import okio.Buffer;
 import retrofit.Call;
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
 import retrofit.http.GET;
 import retrofit.http.Path;
+import retrofit.http.Url;
 
 /**
  * Created by evin on 1/3/16.
@@ -39,14 +45,38 @@ public class RetrieveMovies extends AsyncTask<String, Void, Void>{
 
     @Override
     protected Void doInBackground(String... params) {
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl("http://api.themoviedb.org")
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build();
 
-//        MovieDBService service = retrofit.create(MovieDBService.class);
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+// set your desired log level
+//        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+        OkHttpClient httpClient = new OkHttpClient();
+// add your other interceptors …
+
+// add logging as last interceptor
+        httpClient.interceptors().add(logging);  // <-- this is the important line!
+        httpClient.interceptors().add(new LoggingInterceptor());
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.themoviedb.org")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
+                .build();
+
+        MovieDBService service = retrofit.create(MovieDBService.class);
 //
-//        Call<List<Movie>> listCall = service.listMovies();
+        Call<Page> listCall = service.listMovies();
+        try{
+            Page results = listCall.execute().body();
+            for (Result result: results.getResults()
+                 ) {
+                System.out.println(result.getTitle() + " " + result.getPosterPath());
+            }
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
 //        Log.d(TAG, listCall.toString());
 //        try{
 //            final List<Movie> results;
@@ -83,17 +113,17 @@ public class RetrieveMovies extends AsyncTask<String, Void, Void>{
 //        }catch (Exception e){
 //            Log.d(TAG, "Something didn't go well " + e.toString());
 //        }
-        helloWorld();
+//        helloWorld();
         return null;
     }
 
-//    public interface MovieDBService {
-//        @GET("/3/discover/movie?sort_by=popularity.desc&api_key=" + Constants.MDB_API_KEY)
-//        Call<List<Movie>> listMovies();
-//    }
+    public interface MovieDBService {
+        @GET("/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=100&api_key=" + Constants.MDB_API_KEY)
+        Call<Page> listMovies();
+    }
 
     void helloWorld(){
-        String reader = getURL("http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=100&api_key=fe1a661d36400e9d5663a70bc161c4bc");
+        String reader = getURL("http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=100&api_key=" + Constants.MDB_API_KEY);
 //        try{
 //            JSONObject jsonObject = new JSONObject(reader);
 //            System.out.println("jsonObject = " + jsonObject.getInt("page"));
@@ -110,7 +140,7 @@ public class RetrieveMovies extends AsyncTask<String, Void, Void>{
 ////        System.out.println(r);
 //
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=100&api_key=fe1a661d36400e9d5663a70bc161c4bc");
+        stringBuilder.append("http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=100&api_key=" + Constants.MDB_API_KEY);
 //        stringBuilder.append("?api_key=" + Constants.MDB_API_KEY);
 //        stringBuilder.append("&query=" + query);
 
@@ -190,6 +220,49 @@ public class RetrieveMovies extends AsyncTask<String, Void, Void>{
             return response.body().string();
         }catch (Exception e){
             return "";
+        }
+    }
+
+    public static class LoggingInterceptor implements Interceptor {
+        @Override
+        public com.squareup.okhttp.Response intercept(Interceptor.Chain chain) throws IOException {
+            Log.i("LoggingInterceptor", "inside intercept callback");
+            Request request = chain.request();
+            long t1 = System.nanoTime();
+            String requestLog = String.format("Sending request %s on %s%n%s",
+                    request.url(), chain.connection(), request.headers());
+            if (request.method().compareToIgnoreCase("post") == 0) {
+                requestLog = "\n" + requestLog + "\n" + bodyToString(request);
+            }
+            Log.d("TAG", "request" + "\n" + requestLog);
+            com.squareup.okhttp.Response response = chain.proceed(request);
+            long t2 = System.nanoTime();
+
+            String responseLog = String.format("Received response for %s in %.1fms%n%s",
+                    response.request().url(), (t2 - t1) / 1e6d, response.headers());
+
+            String bodyString = response.body().string();
+
+            Log.d("TAG", "response only" + "\n" + bodyString);
+
+            Log.d("TAG", "response" + "\n" + responseLog + "\n" + bodyString);
+
+            return response.newBuilder()
+                    .body(ResponseBody.create(response.body().contentType(), bodyString))
+                    .build();
+
+        }
+
+
+        public static String bodyToString(final Request request) {
+            try {
+                final Request copy = request.newBuilder().build();
+                final Buffer buffer = new Buffer();
+                copy.body().writeTo(buffer);
+                return buffer.readUtf8();
+            } catch (final IOException e) {
+                return "did not work";
+            }
         }
     }
 
