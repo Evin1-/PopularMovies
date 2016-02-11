dd9fdf8af2da6dde3d66ea1002e85b98537cae37
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-11 11:10:12 -0500

    Add main fragment

---
+++ app/src/main/java/mx/evin/udacity/popularmovies/fragments/MainFragment.java
@@ -0,0 +1,31 @@
+package mx.evin.udacity.popularmovies.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mx.evin.udacity.popularmovies.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
