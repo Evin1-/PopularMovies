8915de7d3c1579f8e728797784ceb0767f3c37fa
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-10 22:50:06 -0500

    Refresh menu icon title and actionBar subtitle

--- app/src/main/java/mx/evin/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/mx/evin/udacity/popularmovies/MainActivity.java
@@ -1,6 +1,7 @@
 package mx.evin.udacity.popularmovies;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     private MoviesAdapter mAdapter;
     private RecyclerView mRecyclerView;
+    private ActionBar mActionBar;
+
+    private String mOrderType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +33,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mResults = new ArrayList<>();
+        mOrderType = "popularity";
 
         mRecyclerView = (RecyclerView) findViewById(R.id.rvMainResults);
+        mActionBar = getSupportActionBar();
         initializeRecycler();
 
         if (savedInstanceState != null && savedInstanceState.containsKey(Constants.mResults_key)) {
             setResults(savedInstanceState.<Result>getParcelableArrayList(Constants.mResults_key));
-            Log.d(TAG, "onCreate: savedInstance Not null");
+            if (savedInstanceState.containsKey(Constants.mOrderType_key)) {
+                mOrderType = savedInstanceState.getString(Constants.mOrderType_key);
+            }
+            Log.d(TAG, "onCreate: savedInstance " + mOrderType);
         } else {
-            queryMovieAPI("popularity");
+            queryMovieAPI(mOrderType);
         }
+
+        refreshActionBar();
+    }
+
+    private void refreshActionBar() {
+        if (mActionBar != null) {
+            mActionBar.setSubtitle("Ordered by " + mOrderType);
+            mActionBar.invalidateOptionsMenu();
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (!mOrderType.equals("popularity")){
+            menu.findItem(R.id.toggle).setTitle(R.string.menuTogglePopularity);
+        }else {
+            menu.findItem(R.id.toggle).setTitle(R.string.menuToggleRating);
+        }
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -53,25 +81,33 @@ public class MainActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.toggle:
-                if (item.getTitle().equals(getString(R.string.menuTogglePopularity))) {
-                    item.setTitle(getString(R.string.menuToggleRating));
-                    queryMovieAPI("popularity");
-                    new RetrieveMovies(this).execute("popularity");
-                } else {
-                    item.setTitle(getString(R.string.menuTogglePopularity));
-                    queryMovieAPI("vote_average");
-                    new RetrieveMovies(this).execute("vote_average");
-                }
+                toggleOrderType(item);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+    private void toggleOrderType(MenuItem item) {
+        if (item.getTitle().equals(getString(R.string.menuTogglePopularity))) {
+            item.setTitle(getString(R.string.menuToggleRating));
+            mOrderType = "popularity";
+        } else {
+            item.setTitle(getString(R.string.menuTogglePopularity));
+            mOrderType = "vote_average";
+        }
+
+        queryMovieAPI(mOrderType);
+        refreshActionBar();
+    }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         Log.d(TAG, "onSaveInstanceState: ");
+
         outState.putParcelableArrayList(Constants.mResults_key, mResults);
+        outState.putString(Constants.mOrderType_key, mOrderType);
+
         super.onSaveInstanceState(outState);
     }
 
@@ -87,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setResults(ArrayList<Result> results) {
-        if (results != null && results.size() > 0 && mResults != null && mAdapter != null){
+        if (results != null && results.size() > 0 && mResults != null && mAdapter != null) {
             mResults.clear();
             mResults.addAll(results);
             mAdapter.notifyDataSetChanged();
