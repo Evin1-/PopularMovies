9807e662088f8515bc8b54a4a912a2befd67ce0d
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-22 15:34:16 -0500

    Add refreshDetails logic to Favorites Activity

--- app/src/main/java/mx/evin/udacity/popularmovies/FavoritesActivity.java
+++ app/src/main/java/mx/evin/udacity/popularmovies/FavoritesActivity.java
@@ -1,25 +1,81 @@
 package mx.evin.udacity.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import mx.evin.udacity.popularmovies.entities.Result;
+import mx.evin.udacity.popularmovies.fragments.FavoritesFragment;
+import mx.evin.udacity.popularmovies.fragments.PlaceholderFragment;
+import mx.evin.udacity.popularmovies.utils.Constants;
 
 public class FavoritesActivity extends AppCompatActivity {
+
+    private PlaceholderFragment mPlaceholderFragment;
+    private FavoritesFragment mFavoritesFragment;
+
+    private Result mResult;
+    private ActionBar mActionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
-            actionBar.setSubtitle(R.string.subtitleFavorites);
+        mFavoritesFragment = (FavoritesFragment) getSupportFragmentManager().findFragmentById(R.id.favoritesFragment);
+        mPlaceholderFragment = (PlaceholderFragment) getSupportFragmentManager().findFragmentById(R.id.placeholderFragment);
+
+        mFavoritesFragment.setRetainInstance(true);
+
+        mActionBar = getSupportActionBar();
+        if (mActionBar != null){
+            mActionBar.setSubtitle(R.string.subtitleFavorites);
+        }
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(Constants.RESULT_TEMP_KEY)) {
+                mResult = savedInstanceState.getParcelable(Constants.RESULT_TEMP_KEY);
+                refreshPlaceholderFragment(mResult);
+            }
         }
     }
 
-    public void refreshDetails(Result result) {
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (mResult != null) {
+            outState.putParcelable(Constants.RESULT_TEMP_KEY, mResult);
+        }
+        super.onSaveInstanceState(outState);
+    }
 
+    private boolean isTabletLayout() {
+        return mPlaceholderFragment != null && mPlaceholderFragment.isAdded();
+    }
+
+    public void refreshDetails(Result result) {
+        if (result == null) {
+            return;
+        }
+
+        mResult = result;
+
+        if (isTabletLayout()) {
+            refreshPlaceholderFragment(mResult);
+        } else {
+            Intent intent = new Intent(this, DetailsActivity.class);
+            Bundle b = new Bundle();
+
+            b.putParcelable(DetailsActivity.MOVIE_PARCELABLE_TAG, mResult);
+            intent.putExtras(b);
+            startActivity(intent);
+        }
+    }
+
+    private void refreshPlaceholderFragment(Result result) {
+        if (mPlaceholderFragment != null) {
+            mPlaceholderFragment.refreshContent(result);
+        }
     }
 }
--- app/src/main/java/mx/evin/udacity/popularmovies/adapters/FavoritesAdapter.java
+++ app/src/main/java/mx/evin/udacity/popularmovies/adapters/FavoritesAdapter.java
@@ -3,7 +3,6 @@ package mx.evin.udacity.popularmovies.adapters;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
