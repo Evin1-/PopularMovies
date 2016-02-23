a0b615a4429cf9e00984eb032c16fd4dd65a36bf
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-23 18:09:57 -0500

    Optimize Imports

--- app/src/main/java/mx/evin/udacity/popularmovies/adapters/VideosAdapter.java
+++ app/src/main/java/mx/evin/udacity/popularmovies/adapters/VideosAdapter.java
@@ -1,13 +1,11 @@
 package mx.evin.udacity.popularmovies.adapters;
 
 import android.content.Context;
-import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
--- app/src/main/java/mx/evin/udacity/popularmovies/utils/NetworkMagic.java
+++ app/src/main/java/mx/evin/udacity/popularmovies/utils/NetworkMagic.java
@@ -1,6 +1,5 @@
 package mx.evin.udacity.popularmovies.utils;
 
-import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
