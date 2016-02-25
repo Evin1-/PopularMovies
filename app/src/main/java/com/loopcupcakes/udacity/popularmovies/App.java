2893eb487a2c36a68851810d7795ca79ed983646
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-24 19:50:56 -0500

    Add Crashlytics support

--- app/src/main/java/com/loopcupcakes/udacity/popularmovies/App.java
+++ app/src/main/java/com/loopcupcakes/udacity/popularmovies/App.java
@@ -3,6 +3,8 @@ package com.loopcupcakes.udacity.popularmovies;
 import android.app.Application;
 
 import com.squareup.leakcanary.LeakCanary;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by evin on 2/11/16.
@@ -11,6 +13,8 @@ public class App extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Fabric.with(this, new Crashlytics());
         LeakCanary.install(this);
     }
 }
