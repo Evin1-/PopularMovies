8382f943afff522082f045e896ef67da8964c60a
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-24 21:50:37 -0500

    Add subtitleRating and subtitlePopularity

--- app/src/main/java/com/loopcupcakes/udacity/popularmovies/MainActivity.java
+++ app/src/main/java/com/loopcupcakes/udacity/popularmovies/MainActivity.java
@@ -161,7 +161,10 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Acti
 
     private void refreshActionBar() {
         if (mActionBar != null) {
-            mActionBar.setSubtitle(getString(R.string.subtitleOrderedPrefix) + mOrderType);
+            mActionBar.setSubtitle(getString(R.string.subtitleOrderedPrefix) +
+                    (mOrderType.equals("popularity")
+                    ? getString(R.string.subtitleRating)
+                    : getString(R.string.subtitlePopularity)));
             mActionBar.invalidateOptionsMenu();
         }
     }
